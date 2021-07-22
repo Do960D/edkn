c20a858a8120e1b6288ce3212aeee9d8d67aa7f4
Ilya <shiparev.ilya@gmail.com>
2021-07-22 16:05:54 +0300

    begin to work with HomeWork 2

---
+++ HW2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+//Home Work 2, Task 1
+
+int main()
+{
+    int16_t si = 65.535;
+    uint32_t i = -2'000'000;
+    int64_t LL = 8'999'999'999;
+    int8_t ch = 'A';
+    bool wobWob = 'true';
+    float pi = 3.14f
+    double dbl = 643525.135326214
+    
+return 0;
+}
