1e5a901b31f028d36bd9ad5cda44f81bf8144941
Ilya <shiparev.ilya@gmail.com>
2021-07-22 17:58:09 +0300

    task1 - Done, task2 - in progress, task3 - in progress

--- HW2.cpp
+++ HW2.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 
-//Home Work 2, Task 1
-
 int main()
 {
+
+//Home Work 2, Task 1
     int16_t si = 65.535;
     uint32_t i = -2'000'000;
     int64_t LL = 8'999'999'999;
     int8_t ch = 'A';
-    bool wobWob = 'true';
-    float pi = 3.14f
-    double dbl = 643525.135326214
+    int8_t ch5 = '5';
+    bool wobWob = true;
+    float pi = 3.14f;
+    double dbl = 5.135326211342154345124;
     
+
+//Home Work 2, Task 3
+    char arr [2][2]=
+     { 
+     '_', '_', '_',
+     '_', '_', '_', 
+     '_', '_', '_' 
+     }
+
 return 0;
 }
+
+//Home Work 2, Task 2
+
+enum weaponry
+{ X, O, _ }
