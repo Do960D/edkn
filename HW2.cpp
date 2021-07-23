e897890f96d3e12e98deb84f2745fbdd42d44510
Ilya <shiparev.ilya@gmail.com>
2021-07-23 17:41:14 +0300

    HW2 Done.

--- HW2.cpp
+++ HW2.cpp
@@ -4,6 +4,7 @@ int main()
 {
 
 //Home Work 2, Task 1
+
     int16_t si = 65.535;
     uint32_t i = -2'000'000;
     int64_t LL = 8'999'999'999;
@@ -12,20 +13,26 @@ int main()
     bool wobWob = true;
     float pi = 3.14f;
     double dbl = 5.135326211342154345124;
-    
-
-//Home Work 2, Task 3
-    char arr [2][2]=
-     { 
-     '_', '_', '_',
-     '_', '_', '_', 
-     '_', '_', '_' 
-     }
-
-return 0;
-}
 
 //Home Work 2, Task 2
 
-enum weaponry
-{ X, O, _ }
+enum XvsO
+    { X, O, _ };
+
+
+//Home Work 2, Task 3
+
+char xo [3][3] ;
+ 
+    xo = { {_, _, _}, {_, _, _}, {_, _, _} } ;
+
+//Home Work 2, Task 4
+
+GameXO field;
+{
+bool figureTrigger = ; // indicate if the cell is filled, prevents re-installation of sign (X\O)
+char xoTriger = ; //indicatet wich sign placed in the current cell, will transmit to "and\or" info to determinate the winner
+};
+    
+return 0;
+}
