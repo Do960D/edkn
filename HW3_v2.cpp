1a286efd62b7a0cfc2fca4c8b3e03a494ac69276
DCLXVI <c0de@code.machine>
2021-07-26 22:00:06 +0300

    HW3_v2 - DONE

--- HW3_v2.cpp
+++ HW3_v2.cpp
@@ -39,25 +39,30 @@ int main()
 	// Home Work 3, Task 3*
 
 
-	int cube[3][3][3] = { {a,b,c}, {b,c,d}, {d,a,d} }, { {a,b,c}, {b,c,d}, {d,a,d} }, { {a,b,c}, {b,c,d}, {d,a,d} }, ;
+	int cube[3][3][3] = {
+	{ {a,b,c}, {b,c,d}, {d,a,d} }, 
+	{ {a,b,c}, {a,c,d}, {d,a,c} }, 
+	{ {a,b,c}, {b,c,d}, {a,a,d} }
+	};
 	int* pCntr = nullptr;               // Declarating a pointer, initialize `em with nullptr (= 0). Denomination. Разименовывание.
 	pCntr = &cube[1][1][1];		   // Declarating an address. It may be address of virible, in this case it is address of array.
-	int cube = *pCntr;			   // A value of the pointer is entered into the variable.
-	cout << "\nCube center is " << cube << endl;
+	int cntr = *pCntr;			   // A value of the pointer is entered into the variable.
+	cout << "\nCube center is " << cntr << endl;
 
 
 	// Home Work 3, Task 4**
 
 	 
-	extern int16_t si = 65.535;
-	extern uint32_t i = -2'000'000;
-	extern float pi = 3.14f;
-	extern double dbl = 5.135326211342154345124;
+	extern uint16_t q;
+	extern uint16_t w;
+	extern uint16_t e;
+	extern uint16_t r;
 
-	float Result = si * (i + (pi / dbl));
+
+	float extResult = q * (w + (static_cast <float> (e) / r));
 
 	cout << "a * (b + (c / d)) = ";
-	cout << Result << endl;
+	cout << extResult << endl;
 
 
 	return 0;
