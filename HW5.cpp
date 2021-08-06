6a2e39d6d7afb3d02ae29eaf0fe9583825968b6c
Шипарев Илья Валерьевич <i.shiparev@VP.local>
2021-08-06 17:08:42 +0300

    in progress

--- HW5.cpp
+++ HW5.cpp
@@ -1,42 +1,52 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <ctime> 
 
-short i;
-const short arrSz = 8;
-
-short stufferFun() 
-{
-
-short x = x + 3;
-
-return 0;
-}
+const int arrSzCnst = 8; 
+ 
 
 
-
-
-int main()
-{
 //Home Work 5, Task 1.
 
 
+double arrDubFun( double * arDub, short arrSz )
+{
+	int i;
+
+	double x = rand() % 1000;
+	arDub;
+	for (i = 0; i < arrSz; i++)
+		arDub[i] = { x };
+	return 0;
+}
+
+int main()
+{
+
+	int i;
+
+	double someArr[arrSzCnst] = { arrDubFun(someArr, arrSzCnst) };
+
+	for (i = 0; i < arrSzCnst; i++)
+
+		std::cout << someArr[i];
+	std::cout << "\n";
 
 
+	// Home Work 5, Task 2.
 
-// Home Work 5, Task 2.
 
-	short arr8[arrSz] = { 0, 0, 1, 0, 0, 1, 0, 0 };
-	for (i = 0; i < 8; i++)
-	arr8[i] == 0 ? (std::cout << ++arr8[i]) : (std::cout << --arr8[i]);
+	uint16_t arr8[arrSzCnst] = { 0, 0, 1, 0, 0, 1, 0, 0 };
 
-//Home Work 5, Task 3.
+	for (i = 0; i < arrSzCnst; i++)
 
-	short arrNew[arrSz] = {};
+		std::cout << arr8[i]; // orign array output
+	std::cout << "\t"; // some space between outputs
 
-	do (x = 1, arrNew[i] = stufferFun());
-	while (arrNew[i] == arrSz);
+	for (i = 0; i < arrSzCnst; i++)
 
-	for (i = 0; i < 8; i++)
-	std::cout << arrNew[i];
+		arr8[i] == 0 ?
+		(std::cout << ++arr8[i]) : (std::cout << --arr8[i]); // ternar for reversing orign array and instant output the result 
 
-return 0;
+	return 0;
 }
