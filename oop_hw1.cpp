68a9cbd2a0e4ff51756bd94257e14eea5dd2d319
Твин Гарри <phuqup@bk.ru>
2022-02-08 14:43:12 +0300

    test VSCode GitHub push

--- oop_hw1.cpp
+++ oop_hw1.cpp
@@ -1,8 +1,9 @@
 ﻿#include <iostream>
+#include <cstdint>
 
 //TASK 1
 
-class Power 
+class Power
 {
 private:
 
@@ -11,15 +12,15 @@ private:
 
 public:
 
-	void setVar(int var1, int var2) { m_var1 = var1; m_var2 = var2;}
+	void setVar(int var1, int var2) { m_var1 = var1; m_var2 = var2; }
 
-	int calculate(int m_var1 = 2, int m_var2 = 3) 
+	int calculate(int m_var1 = 2, int m_var2 = 3)
 	{
 		int num = m_var1;
 		int pow = m_var2;
 		int result = 1;
 
-		for (int i = 0; i < pow ; ++i)
+		for (int i = 0; i < pow; ++i)
 		{
 			result *= num;
 		}
@@ -27,11 +28,63 @@ public:
 	}
 };
 
+
+//TASK 2
+
+
+class RGBA
+{
+
+	std::uint8_t m_red, m_green, m_blue, m_alpha;
+
+public:
+
+	RGBA()
+	{
+		m_red = 0;
+		m_green = 0;
+		m_blue = 0;
+		m_alpha = 255;
+	}
+
+	RGBA(int red, int green, int blue, int alpha)
+	{
+		m_red = red;
+		m_green = green;
+		m_blue = blue;
+		m_alpha = alpha;
+
+	}
+
+	void print()
+	{
+
+		std::cout << m_red << m_green << m_blue << m_alpha;
+
+	};
+
+};
+
+
 int main()
 {
+
+	// TASK 1
+
 	Power orignOne;
 
-	orignOne.setVar(5,10);
+	orignOne.setVar(5, 10);
 	std::cout << orignOne.calculate();
+
+	//TASK 2
+
+	RGBA lightsUp;
+
+	lightsUp();
+	lightsUp(10, 10, 10, 150);
+	lightsUp.print();
+
 }
+
+
 
