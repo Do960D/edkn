b116bbd8e064567f642be9a1e5f0a32b0883be35
Твин Гарри <phuqup@bk.ru>
2022-02-09 17:32:40 +0300

    +task2 (preproduction) +task3(in progress)

--- oop_hw1.cpp
+++ oop_hw1.cpp
@@ -3,12 +3,13 @@
 
 //TASK 1
 
-class Power
-{
+class Power {
+
 private:
 
 	int m_var1;
 	int m_var2;
+
 
 public:
 
@@ -31,38 +32,115 @@ public:
 
 //TASK 2
 
+class RGBa {
 
-class RGBA
-{
+private:
 
-	std::uint8_t m_red, m_green, m_blue, m_alpha;
+	uint8_t m_red;
+	uint8_t m_green;
+	uint8_t m_blue;
+	uint8_t m_alpha;
+
 
 public:
 
-	RGBA()
+	RGBa() : m_red (0), m_green (0), m_blue (0), m_alpha (255)
 	{
-		m_red = 0;
-		m_green = 0;
-		m_blue = 0;
-		m_alpha = 255;
+		m_red;
+		m_green;
+		m_blue;
+		m_alpha;
 	}
 
-	RGBA(int red, int green, int blue, int alpha)
+	void print( uint8_t m_red = 0, uint8_t m_green = 0, uint8_t m_blue = 0, uint8_t m_alpha = 255)
 	{
-		m_red = red;
-		m_green = green;
-		m_blue = blue;
-		m_alpha = alpha;
-
-	}
-
-	void print()
-	{
-
 		std::cout << m_red << m_green << m_blue << m_alpha;
+	}
 
-	};
 
+};
+
+
+//TASK 3
+
+class Stack {
+
+private:
+
+	size_t size = 10;
+	
+	int* privateArray = new int[size];
+
+
+public:
+
+	void reset() 
+	{
+		for (size_t i = 0; i < size; ++i)
+			privateArray[i] = 0;		
+			
+		size = 0;
+
+		std::cout << "Stak is reseted!\n";
+
+	}
+
+	bool push(int x)
+	{
+		bool isFull = false;
+		if (size == 0)
+			size = 10;
+
+		for (size_t i = 0; i < size; ++i)
+		{
+			if (privateArray[i] == 0)
+				privateArray[i] = x;
+
+			
+			if (i == size && privateArray[i] != 0)
+				isFull = true;
+
+		}
+
+		return isFull;
+	}
+
+	void pop()
+	{
+		size_t i;
+		size_t emptyness = 0;
+
+		for (size_t i = 0; i < size; ++i)
+		{
+			if (privateArray[i] == 0)
+				++emptyness;
+		}
+
+		if (emptyness == size)
+			std::cout << "Stack is empty!\n";
+
+		std::cout << "POP in progress: \n";
+
+		for (i = 0; i < size; ++i)
+			std::cout << privateArray[i] << "\n";
+
+		std::cout << std::endl;
+	}
+
+	void print() 
+	{
+		std::cout << "Print in progress: \n";
+
+		for (size_t i = 0; i < size; ++i)
+		{
+			if (privateArray[i] == 0)
+				break;
+
+			std::cout <<  privateArray[i] << "\n";
+		}
+
+		std::cout << "End of array!\n" << std::endl;
+	}
 };
 
 
@@ -74,16 +152,33 @@ int main()
 	Power orignOne;
 
 	orignOne.setVar(5, 10);
-	std::cout << orignOne.calculate();
+	std::cout << orignOne.calculate() << std::endl;
 
 	//TASK 2
 
-	RGBA lightsUp;
+	RGBa monitor;
 
-	lightsUp();
-	lightsUp(10, 10, 10, 150);
-	lightsUp.print();
+	monitor.print();
 
+	//TASK 3
+
+	Stack stack;
+
+	stack.reset();
+	stack.print();
+
+	stack.push(3);
+	stack.push(7);
+	stack.push(5);
+	stack.print();
+
+	stack.pop();
+	stack.print();
+
+	stack.pop();
+	stack.pop();
+	stack.print();
+	
 }
 
 
