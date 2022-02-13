8f8189a89515f90039af934b86445771d161ffe8
Ilya <87614970+Do960D@users.noreply.github.com>
2022-02-13 10:38:23 +0300

    task1 in progress

---
+++ oop_hw2.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+
+class Person {
+public:
+
+	Person(short age,short weight, uint8_t sex, std::string name) 
+	{
+		this->age;
+		this->weight;
+		this->sex;
+		this-> name;
+	}
+
+	void setName(std::string name) 
+	{
+		this -> name = name;
+	}
+
+	void setWeightAge (short weight, short age) 
+	{
+		this -> weight = weight;
+		this -> age = age;
+	}
+
+private:
+	std::string name;
+	uint8_t sex;
+	short age, weight;
+
+};
+
+class Student : public Person {
+public:
+
+	Student(short age, short weight, uint8_t sex, std::string name)
+	{
+		this->id++;
+		this->studYear = studYear;
+	}
+
+private:
+
+	short studYear, id = 0;
+
+};
+
+void main() 
+{
+	Person test;
+	test.setName("test");
+	test.setWeightAge(70, 25);
+
+
+}
